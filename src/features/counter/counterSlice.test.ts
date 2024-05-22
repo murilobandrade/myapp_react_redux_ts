1142009bbbb6e72a8dfaa6098ea3329a917d4b6e
Murilo Borges de Andrade <murilo.b.andrade@gmail.com>
2024-05-22 18:55:29 -0300

    Remove unnecessary redux counter state and action
     - remove redux state status
     - remove redux action decrement, incrementByAmount, incrementAsync
     - remove unused redux thunk incrementIfOdd
     - refact UI (App and Counter) to remove old references to removed redux state/action

--- src/features/counter/counterSlice.test.ts
+++ src/features/counter/counterSlice.test.ts
@@ -3,9 +3,7 @@ import { makeStore } from "../../app/store"
 import type { CounterSliceState } from "./counterSlice"
 import {
   counterSlice,
-  decrement,
   increment,
-  incrementByAmount,
   selectCount,
 } from "./counterSlice"
 
@@ -17,7 +15,6 @@ describe<LocalTestContext>("counter reducer", it => {
   beforeEach<LocalTestContext>(context => {
     const initialState: CounterSliceState = {
       value: 3,
-      status: "idle",
     }
 
     const store = makeStore({ counter: initialState })
@@ -28,7 +25,6 @@ describe<LocalTestContext>("counter reducer", it => {
   it("should handle initial state", () => {
     expect(counterSlice.reducer(undefined, { type: "unknown" })).toStrictEqual({
       value: 0,
-      status: "idle",
     })
   })
 
@@ -40,19 +36,4 @@ describe<LocalTestContext>("counter reducer", it => {
     expect(selectCount(store.getState())).toBe(4)
   })
 
-  it("should handle decrement", ({ store }) => {
-    expect(selectCount(store.getState())).toBe(3)
-
-    store.dispatch(decrement())
-
-    expect(selectCount(store.getState())).toBe(2)
-  })
-
-  it("should handle incrementByAmount", ({ store }) => {
-    expect(selectCount(store.getState())).toBe(3)
-
-    store.dispatch(incrementByAmount(2))
-
-    expect(selectCount(store.getState())).toBe(5)
-  })
 })
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -5,12 +5,10 @@ import { fetchCount } from "./counterAPI"
 
 export interface CounterSliceState {
   value: number
-  status: "idle" | "loading" | "failed"
 }
 
 const initialState: CounterSliceState = {
   value: 0,
-  status: "idle",
 }
 
 // If you are not using async thunks you can use the standalone `createSlice`.
@@ -27,63 +25,16 @@ export const counterSlice = createAppSlice({
       // immutable state based off those changes
       state.value += 1
     }),
-    decrement: create.reducer(state => {
-      state.value -= 1
-    }),
-    // Use the `PayloadAction` type to declare the contents of `action.payload`
-    incrementByAmount: create.reducer(
-      (state, action: PayloadAction<number>) => {
-        state.value += action.payload
-      },
-    ),
-    // The function below is called a thunk and allows us to perform async logic. It
-    // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-    // will call the thunk with the `dispatch` function as the first argument. Async
-    // code can then be executed and other actions can be dispatched. Thunks are
-    // typically used to make async requests.
-    incrementAsync: create.asyncThunk(
-      async (amount: number) => {
-        const response = await fetchCount(amount)
-        // The value we return becomes the `fulfilled` action payload
-        return response.data
-      },
-      {
-        pending: state => {
-          state.status = "loading"
-        },
-        fulfilled: (state, action) => {
-          state.status = "idle"
-          state.value += action.payload
-        },
-        rejected: state => {
-          state.status = "failed"
-        },
-      },
-    ),
   }),
   // You can define your selectors here. These selectors receive the slice
   // state as their first argument.
   selectors: {
     selectCount: counter => counter.value,
-    selectStatus: counter => counter.status,
   },
 })
 
 // Action creators are generated for each case reducer function.
-export const { decrement, increment, incrementByAmount, incrementAsync } =
-  counterSlice.actions
+export const { increment } = counterSlice.actions
 
 // Selectors returned by `slice.selectors` take the root state as their first argument.
-export const { selectCount, selectStatus } = counterSlice.selectors
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
-  (amount: number): AppThunk =>
-  (dispatch, getState) => {
-    const currentValue = selectCount(getState())
-
-    if (currentValue % 2 === 1 || currentValue % 2 === -1) {
-      dispatch(incrementByAmount(amount))
-    }
-  }
+export const { selectCount } = counterSlice.selectors
